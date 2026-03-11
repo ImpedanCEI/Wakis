:bug: fix indexing in transverse wake potential calculation affecting non-uniform grids (#65)
The new implementation uses the local transverse coordinates from the Ez field monitor `xf` and `yf` to compute the gradient mesh steps

* Fix: Index bug in wakeSolver.py Wpx and Wpy

* Fix transverse wake potential calculation in wakeSolver. Adjustment of the dx and dy from xf and yf

* Refine transverse wake potential fix, adjust dx->dxf and dy->dyf. dxf and dyf are from np.diff(self.xf) and np.diff(self.yf)

wakis/wakeSolver.py
@@ -593,6 +593,9 @@ class WakeSolver:
         WPx = np.zeros_like(self.s)
         WPy = np.zeros_like(self.s)
         int_WP = np.zeros_like(self.WP_3d)
+        
+        dxf = np.diff(self.xf)
+        dyf = np.diff(self.yf)
 
         print("Calculating transverse wake potential WPx, WPy...")
         # Obtain the transverse wake potential
@@ -607,11 +610,11 @@ class WakeSolver:
                         pbar.update(1)
 
                 # Perform the gradient (second order scheme)
-                WPx[n] = -(int_WP[i0 + 1, j0, n] - int_WP[i0 - 1, j0, n]) / (
-                    self.dx[i0 - 1] + self.dx[i0]
+                WPx[n] = -0.5*(int_WP[i0 + 1, j0, n] - int_WP[i0 - 1, j0, n]) / (
+                    dxf[i0 - 1] + dxf[i0]
                 )
-                WPy[n] = -(int_WP[i0, j0 + 1, n] - int_WP[i0, j0 - 1, n]) / (
-                    self.dy[j0 - 1] + self.dy[j0]
+                WPy[n] = -0.5*(int_WP[i0, j0 + 1, n] - int_WP[i0, j0 - 1, n]) / (
+                    dyf[j0 - 1] + dyf[j0]
                 )
 
         self.WPx = WPx